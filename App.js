65396794882fb2635f9349d11f1b2e90905034b1
Guilherme Santos <110691686+GuiSantosMacedo1@users.noreply.github.com>
2023-01-04 15:46:11 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
+import "./styles/global.css"
+
+function App(){
+    return (
+        <>
+        <Header></Header>
+        <Layout></Layout>
+        </>
+    )
+}
+export default App;
